69b7628f184033bdf6564d95b7c4dc881e5f7243
belen <belen@belen.com>
2020-02-18 16:07:30 +0100

    dfdf

--- main.c
+++ main.c
@@ -42,13 +42,14 @@ int is_armstrong_number(int );
  * Estoy documentando main
  *
  */
-void main(){   
+int main(){   
 	int n;   
 	printf("Introduce un número entero");   
 	scanf("%d",&n);   
 	if (is_armstrong_number(n))      
 		printf("%d es un número de armstrong \U0001F60E \n", n);   
 	else printf("%d no es un número de armstrong \U0001F622 \n", n);
+	return 0;
 }
 /**
  * 
